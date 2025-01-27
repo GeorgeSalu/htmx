65d1ffc032ab334b05ce9a9cd90af8e411d43eee
George Salu <george.salu10@gmail.com>
2025-01-27 14:21:08 -0300

    fundamentos-htmx - todo-list - crindo a operacao de criacao de uma TODO

--- fundamentos-htmx/projeto/app.js
+++ fundamentos-htmx/projeto/app.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
 
-
+// POST => /api/todos => criar um novo todo
 app.use("/api", todoRoutes)
 
 app.listen(PORT, () => {
