ee50f98ed0cdb7703bf7860efe5caea6f90d2c04
George Salu <george.salu10@gmail.com>
2025-01-27 15:05:42 -0300

    fundamentos-htmx - todo-list - carregando todos ja existentes no load da pagina

--- fundamentos-htmx/projeto/routes/todoRoutes.js
+++ fundamentos-htmx/projeto/routes/todoRoutes.js
@@ -4,6 +4,29 @@ const router = express.Router();
 const Todo = require("../model/Todo");
 const { Sequelize } = require("sequelize");
 
+const createTodoTemplate = (todo) => `
+    <li id="todo-${todo.id}" class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo.title}</span>
+        <div>
+            <form class="d-inline">
+                <button>${todo.completed ? "Desmarcar" : "Marcar conclusão"}</button>
+            </form>
+            <form class="d-inline">
+                <button class="btn btn-danger">Excluir</button>
+            </form>
+        </div>
+    </li>
+`
+
+router.get("/todos", async(req, res) => {
+
+    const todos = await Todo.findAll();
+
+    const todoItems = todos.map(createTodoTemplate).join("");
+
+    res.send(todoItems)
+})
+
 router.post("/todos", async (req, res) => {
     const { title } = req.body;
 
