ff26437b95ef09171fecb05225a53f77a1f729c5
George Salu <george.salu10@gmail.com>
2025-02-16 14:27:42 -0300

    eventos - antes e depois do evento do swap de conteudo

--- hora-de-codar/5_eventos/script.js
+++ hora-de-codar/5_eventos/script.js
@@ -1,16 +1,20 @@
 /*
-evento de load
-document.body.addEventListener("htmx:load", function(event) {
-    console.log(event);
+    evento de load
+    document.body.addEventListener("htmx:load", function(event) {
+        console.log(event);
 
-    console.log("carregou a pagina")
-});
+        console.log("carregou a pagina")
+    });
 */
 
-document.body.addEventListener("htmx:beforeSwap", function(event) {
-    console.log("antes do swap")
-});
+/*
+    eventos de antes e depois do swap
+    document.body.addEventListener("htmx:beforeSwap", function(event) {
+        console.log("antes do swap")
+    });
 
-document.body.addEventListener("htmx:afterSwap", function(event) {
-    console.log("depois do swap")
-})
+    document.body.addEventListener("htmx:afterSwap", function(event) {
+        console.log("depois do swap")
+    })
+*/
+
