005ca260c92c6a46421cf98dfacb2beacdd9b412
George Salu <george.salu10@gmail.com>
2025-02-22 13:11:10 -0300

    filesharer - upload de arquivo parte 1

--- hora-de-codar/6_filesharer/routes/admin.js
+++ hora-de-codar/6_filesharer/routes/admin.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const upload = require("../config/multerConfig");
 
 const { User } = require("../models");
+const { File } = require("../models");
+
 const isAuthenticated = require("../middleware/isAuthenticated");
 
 const router = express.Router();
@@ -13,7 +16,26 @@ router.get("/", isAuthenticated, (req, res) => {
 router.get("/all-files", isAuthenticated, (req, res) => {
 
     res.render("layout", { title: "Biblioteca de arquivos", template: "allfiles", userId: req.session.userId, files: [] })
-})
+});
+
+router.get("/upload", isAuthenticated, upload.single("arquivo") ,(req, res) => {
+
+    const { nome, descricao } = req.body;
+    const caminho = req.file.path;
+    const userId = req.session.userId;
+
+    if(!nome || !file) {
+
+        res.status(422).send("Preencha todos os campos")
+    }
+
+    try {
+        
+    } catch (error) {
+        res.status(500).send("Preencha todos os campos")
+    }
+
+});
 
 
 module.exports = router;
