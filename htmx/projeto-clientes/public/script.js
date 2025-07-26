f24e6ba0a3441d5d5a58388a132feb7c40882b7e
George Salu <george.salu10@gmail.com>
2025-07-26 18:50:21 -0300

    htmx completo - secao 5 - quando um funcionario e cadastrado atualiza a listagem de funcionarios

--- htmx/projeto-clientes/public/script.js
+++ htmx/projeto-clientes/public/script.js
@@ -1,3 +1,16 @@
+document.addEventListener("htmx:afterRequest", async function(event) {
+
+    if(event.target.getAttribute("id") === 'form-clientes') {
+        await fetchClientes();
+    }
+
+})
+
+async function fetchClientes() {
+    await htmx.ajax("GET", "http://localhost:3333/clientes", "#lista-clientes")
+}
+
+
 document.addEventListener("htmx:configRequest", function(event) {
 
     // intercepta as requisicoes para rota /clientes que seja diferentes do metodo get
