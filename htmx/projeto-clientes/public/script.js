7b04694aca53e9c95cb850dadf67bad0713a3168
George Salu <george.salu10@gmail.com>
2025-07-26 11:34:30 -0300

    htmx completo - secao 5 - melhorando efeito toast

--- htmx/projeto-clientes/public/script.js
+++ htmx/projeto-clientes/public/script.js
@@ -1,9 +1,10 @@
 document.addEventListener("htmx:configRequest", function(event) {
 
+    // intercepta as requisicoes para rota /clientes que seja diferentes do metodo get
     if(event.detail.path === '/clientes' && event.detail.verb !== "get") {
         setTimeout(() => {
             document.querySelector("#toast").innerHTML = '';
-        }, 3000)
+        }, 2000)
     }
 
 })
