36b9c3ba6df4b68bf397fd3183c9673e2ad66a8a
George Salu <george.salu10@gmail.com>
2025-07-29 09:35:14 -0300

    htmx completo - secao 6 - corrige configuracao do tailwindcss

--- htmx/projeto-devlink/tailwind.config.js
+++ htmx/projeto-devlink/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [".views/**/*.ejs"],
+module.exports = {
+  content: ["./views/**/*.ejs"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
