cf63d5c9eeabf68f00d7eb84a0b50bd35cfaf496
George Salu <george.salu10@gmail.com>
2025-07-31 11:43:41 -0300

    htmx completo - secao 6 - criando operacao de cadastro de usuario

--- htmx/projeto-devlink/routes/authetication.js
+++ htmx/projeto-devlink/routes/authetication.js
@@ -13,4 +13,32 @@ router.get("/register", (req, res) => {
     res.render("layout", { title: "Pagina novo usuario - devlinks", template: "register" })
 })
 
+router.post("/register", async (req, res) => {
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password) {
+        return res.status(400).send("Preencha todos os campos")
+    }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    try {
+
+        const user = await User.create({
+            name,
+            email,
+            password: hash
+        })
+
+        console.log(user);
+
+        res.send("user criado")
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send("erro ao registrar usuario")
+    }
+
+})
+
 module.exports = router
