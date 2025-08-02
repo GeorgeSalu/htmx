1742f9eeda3d2e59b3f7b8637b76ee8ede5596e6
George Salu <george.salu10@gmail.com>
2025-08-02 10:51:42 -0300

    htmx completo - secao 6 - corrige title template

--- htmx/projeto-devlink/routes/dashboard.js
+++ htmx/projeto-devlink/routes/dashboard.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/dashboard", (req, res) => {
-    res.render("layout", { title: "devLinks - Painel administrativo", template: 'dashboard' })
+    res.render("layout", { title: "DevLinks - Painel administrativo", template: 'dashboard' })
 })
 
 module.exports = router;
