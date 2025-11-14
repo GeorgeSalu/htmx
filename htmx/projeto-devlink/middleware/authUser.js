0d6c64d77c0de0652279af0b139917f86b8268df
George Salu <george.salu10@gmail.com>
2025-11-14 17:39:58 -0300

    htmx completo - secao 6 - protegendo rota admin #01

--- htmx/projeto-devlink/middleware/authUser.js
+++ htmx/projeto-devlink/middleware/authUser.js
@@ -3,7 +3,14 @@ const jwt = require("jsonwebtoken");
 function autheticationToken(req, res, next) {
     const token = req.cookies?.auth_token
 
+    if (token === null || !token) {
+        res.setHeader("HX-Redirect", "/")
+        return res.redirect("/")
+    }
+
     console.log(token)
+
+    next()
 }
 
 module.exports = autheticationToken
--- htmx/projeto-devlink/routes/dashboard.js
+++ htmx/projeto-devlink/routes/dashboard.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const authenticationToken = require("../middleware/authUser")
 
 const router = express.Router();
 
-router.get("/dashboard", (req, res) => {
+router.get("/dashboard", authenticationToken, (req, res) => {
     res.render("layout", { title: "DevLinks - Painel administrativo", template: 'dashboard' })
 })
 
