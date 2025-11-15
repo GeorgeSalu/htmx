95e3f934890351e3ea487177fe6243dd313b739f
George Salu <george.salu10@gmail.com>
2025-11-15 09:25:46 -0300

    htmx completo - secao 6 - corrige title rota dashboard.js

--- htmx/projeto-devlink/routes/dashboard.js
+++ htmx/projeto-devlink/routes/dashboard.js
@@ -4,7 +4,7 @@ const authenticationToken = require("../middleware/authUser")
 const router = express.Router();
 
 router.get("/dashboard", authenticationToken, (req, res) => {
-    res.render("layout", { title: "DevLinks - Painel administrativo", template: 'dashboard' })
+    res.render("layout", { title: "DevLinks - Painel Administrativo", template: 'dashboard' })
 })
 
 module.exports = router;
