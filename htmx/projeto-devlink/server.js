21b18ce3f581847eb1a6c213f77f69cb35279f8f
George Salu <george.salu10@gmail.com>
2025-11-14 22:39:26 -0300

    htmx completo - secao 6 - corrige nome routes authentication.js

--- htmx/projeto-devlink/server.js
+++ htmx/projeto-devlink/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
-const authRoutes = require("./routes/authetication")
+const authRoutes = require("./routes/authentication")
 const dashboardRoutes = require("./routes/dashboard")
 
 const app = express()
