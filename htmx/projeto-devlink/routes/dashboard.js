24639370f1aaaef348ba20de10bf4752f4fd2c72
George Salu <george.salu10@gmail.com>
2025-11-19 12:28:32 -0300

    htmx completo - secao 6 - cadastrando link na api #02

--- htmx/projeto-devlink/routes/dashboard.js
+++ htmx/projeto-devlink/routes/dashboard.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authenticationToken = require("../middleware/authUser")
+const { Links } = require("../models")
 
 const router = express.Router();
 
@@ -7,12 +8,21 @@ router.get("/dashboard", authenticationToken, (req, res) => {
     res.render("layout", { title: "DevLinks - Painel Administrativo", template: 'dashboard' })
 })
 
-router.post("/create-link", authenticationToken,  (req, res) => {
+router.post("/create-link", authenticationToken, async  (req, res) => {
     const { name, url } = req.body
     const userId = req.userId
 
-    console.log(name, url)
-    console.log(userId)
+    try {
+
+        const link = await Links.create({
+            name,
+            url,
+            userId
+        })
+
+    }catch(err) {
+        return res.status(400).send("Erro ao registrar o link")
+    }
 
     res.send("ok")
 })
