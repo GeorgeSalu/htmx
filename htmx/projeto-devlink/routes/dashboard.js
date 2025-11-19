ffc529b732164c2d59359fe61548ffbccc4c92cd
George Salu <george.salu10@gmail.com>
2025-11-19 10:29:29 -0300

    htmx completo - secao 6 - cadastrando link na api #01

--- htmx/projeto-devlink/routes/dashboard.js
+++ htmx/projeto-devlink/routes/dashboard.js
@@ -7,4 +7,14 @@ router.get("/dashboard", authenticationToken, (req, res) => {
     res.render("layout", { title: "DevLinks - Painel Administrativo", template: 'dashboard' })
 })
 
+router.post("/create-link", authenticationToken,  (req, res) => {
+    const { name, url } = req.body
+    const userId = req.userId
+
+    console.log(name, url)
+    console.log(userId)
+
+    res.send("ok")
+})
+
 module.exports = router;
