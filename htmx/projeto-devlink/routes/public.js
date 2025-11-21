964b6d19301f5696247e46a33627cf751aa66dcd
George Salu <george.salu10@gmail.com>
2025-11-21 13:16:41 -0300

    htmx completo - secao 6 - criando a pagina home #02

---
+++ htmx/projeto-devlink/routes/public.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { Links } = require("../models")
+
+const router = express.Router();
+
+router.get("/links", async (req, res) => {
+    
+    try {
+
+        const links = await Links.findAll();
+
+        let html = links.map((link) => `
+            <a class='bg-white flex items-center justify-center w-full p-2 mt-4 rounded'
+                href="${link.url}" target='_blank'>
+                <p class='text-lg text-black w-full text-center'>${link.name}</p>
+            </a>
+        `).join("")
+
+        return res.send(html)
+
+    }catch(err) {
+        console.log(err)
+        res.status(400).send("Erro ao localizar seus links")
+    }
+    
+})
+
+module.exports = router
